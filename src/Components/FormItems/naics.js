7cec59f823e429a9b9c2782715131aaa72ce7995
usayyed <ulfatsayyed@outlook.com>
2020-04-13 23:51:30 -0400

    Added links to NAICS and SIC codes

--- src/Components/FormItems/naics.js
+++ src/Components/FormItems/naics.js
@@ -1,90 +1,120 @@
-import React, { Component } from 'react';
-import { Input, Form, Card, Button } from 'element-react';
-import MultiOptionCard from '../multiOptionCard';
-import SingleOptionCard from '../singleOptionCard';
-
+import React, { Component } from "react";
+import { Input, Form, Card, Button } from "element-react";
+import MultiOptionCard from "../multiOptionCard";
+import SingleOptionCard from "../singleOptionCard";
 
 class NaicsFormItem extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.onAdd = this.onAdd.bind(this)
-        this.onDelete = this.onDelete.bind(this)
+    this.onAdd = this.onAdd.bind(this);
+    this.onDelete = this.onDelete.bind(this);
 
-        this.state = {
-            naicsCodes: [],
-            count: 0,
-            min: 1,
-            max: 5
-        }
-    }
+    this.state = {
+      naicsCodes: [],
+      count: 0,
+      min: 1,
+      max: 5,
+    };
+  }
 
-    onAdd() {
-        this.state.naicsCodes.push({
-            value: ''
-        })
-        this.setState({
-            ...this.state,
-            count: this.state.count + 1
-        })
-        this.forceUpdate()
-        this.props.onUpdate('naicsCodes', this.state.naicsCodes)
-    }
+  onAdd() {
+    this.state.naicsCodes.push({
+      value: "",
+    });
+    this.setState({
+      ...this.state,
+      count: this.state.count + 1,
+    });
+    this.forceUpdate();
+    this.props.onUpdate("naicsCodes", this.state.naicsCodes);
+  }
 
-    onDelete(index) {
-        const naicsCodesList = this.state.naicsCodes.filter((s, i) => i !== index)
+  onDelete(index) {
+    const naicsCodesList = this.state.naicsCodes.filter((s, i) => i !== index);
 
-        this.setState({
-            ...this.state,
-            naicsCodes: naicsCodesList,
-            count: this.state.count - 1
-        })
+    this.setState({
+      ...this.state,
+      naicsCodes: naicsCodesList,
+      count: this.state.count - 1,
+    });
 
-        this.forceUpdate()
-        this.props.onUpdate('naicsCodes', naicsCodesList)
-    }
+    this.forceUpdate();
+    this.props.onUpdate("naicsCodes", naicsCodesList);
+  }
 
-    onChange(index, key, value) {
-        this.state.naicsCodes[index][key] = value
-        this.forceUpdate()
-        this.props.onUpdate('naicsCodes', this.state.naicsCodes)
-    }
+  onChange(index, key, value) {
+    this.state.naicsCodes[index][key] = value;
+    this.forceUpdate();
+    this.props.onUpdate("naicsCodes", this.state.naicsCodes);
+  }
 
-    render() {
-        return (
-            <div>
-            <MultiOptionCard title="List your NAICS Codes" onAdd={this.onAdd} min={this.state.min} max={this.state.max} count={this.state.count}>
-                {
-                    this.state.naicsCodes.map((naicsCode, index) => {
-                        return (
-                            <Card key={index}>
-                                <Form.Item
-                                    key={index}
-                                    label={`NAICS Code ${index + 1}`}
-                                    prop={`naicsCodes:${index}`}
-                                    rules={{
-                                        type: 'object',
-                                        fields: {
-                                            value: [{ required: true, message: 'NAICS Code can not be empty', trigger: 'blur'},{ trigger: 'blur', validator: (rule, value, callback) => {
-                                            var naicsPattern = /^\d{6}$/
-                                            if (!naicsPattern.test(value)) {
-                                            callback(new Error('NAICS# error: Invalid NAICS number. Please Enter 6 digit NAICS number'));
-                                            } else {
-                                            callback();
-                                            }}}]
-                                        }
-                                    }}
-                                >
-                                <SingleOptionCard onDelete={this.onDelete} index={index} disabledDelete={this.state.count <= this.state.min}>
-                                        <Input value={naicsCode.value} onChange={(value) => this.onChange(index, 'value', value)}></Input>
-                                    </SingleOptionCard>
-                                </Form.Item>
-                            </Card>
-                        )
-                    })
-                }
-            </MultiOptionCard>
-            <div className="add-btn">
+  render() {
+    return (
+      <div>
+        <MultiOptionCard
+          title="List your NAICS Codes"
+          onAdd={this.onAdd}
+          min={this.state.min}
+          max={this.state.max}
+          count={this.state.count}
+        >
+          <div className="form-link">
+            <a href="https://www.naics.com/search/" target="_blank">
+              NAICS Codes
+            </a>
+          </div>
+          {this.state.naicsCodes.map((naicsCode, index) => {
+            return (
+              <Card key={index}>
+                <Form.Item
+                  key={index}
+                  label={`NAICS Code ${index + 1}`}
+                  prop={`naicsCodes:${index}`}
+                  rules={{
+                    type: "object",
+                    fields: {
+                      value: [
+                        {
+                          required: true,
+                          message: "NAICS Code can not be empty",
+                          trigger: "blur",
+                        },
+                        {
+                          trigger: "blur",
+                          validator: (rule, value, callback) => {
+                            var naicsPattern = /^\d{6}$/;
+                            if (!naicsPattern.test(value)) {
+                              callback(
+                                new Error(
+                                  "NAICS# error: Invalid NAICS number. Please Enter 6 digit NAICS number"
+                                )
+                              );
+                            } else {
+                              callback();
+                            }
+                          },
+                        },
+                      ],
+                    },
+                  }}
+                >
+                  <SingleOptionCard
+                    onDelete={this.onDelete}
+                    index={index}
+                    disabledDelete={this.state.count <= this.state.min}
+                  >
+                    <Input
+                      value={naicsCode.value}
+                      onChange={(value) => this.onChange(index, "value", value)}
+                    ></Input>
+                  </SingleOptionCard>
+                </Form.Item>
+              </Card>
+            );
+          })}
+        </MultiOptionCard>
+        <div className="add-btn">
           <Button
             type="primary"
             icon="plus"
@@ -95,9 +125,8 @@ class NaicsFormItem extends Component {
           ></Button>
         </div>
       </div>
-        )
-    }
-
+    );
+  }
 }
 
-export default NaicsFormItem; 
+export default NaicsFormItem;
--- src/Components/FormItems/sic.js
+++ src/Components/FormItems/sic.js
@@ -1,91 +1,120 @@
-import React, { Component } from 'react';
-import { Input, Form, Card, Button } from 'element-react';
-import MultiOptionCard from '../multiOptionCard';
-import SingleOptionCard from '../singleOptionCard';
-
+import React, { Component } from "react";
+import { Input, Form, Card, Button } from "element-react";
+import MultiOptionCard from "../multiOptionCard";
+import SingleOptionCard from "../singleOptionCard";
 
 class SicFormItem extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.onAdd = this.onAdd.bind(this)
-        this.onDelete = this.onDelete.bind(this)
+    this.onAdd = this.onAdd.bind(this);
+    this.onDelete = this.onDelete.bind(this);
 
-        this.state = {
-            sicCodes: [],
-            count: 0,
-            min: 1,
-            max: 5
-        }
-    }
+    this.state = {
+      sicCodes: [],
+      count: 0,
+      min: 1,
+      max: 5,
+    };
+  }
 
-    onAdd() {
-        this.state.sicCodes.push({
-            value: ''
-        })
-        this.setState({
-            ...this.state,
-            count: this.state.count + 1
-        })
-        this.forceUpdate()
-        this.props.onUpdate('sicCodes', this.state.sicCodes)
-    }
+  onAdd() {
+    this.state.sicCodes.push({
+      value: "",
+    });
+    this.setState({
+      ...this.state,
+      count: this.state.count + 1,
+    });
+    this.forceUpdate();
+    this.props.onUpdate("sicCodes", this.state.sicCodes);
+  }
 
-    onDelete(index) {
-        const sicCodesList = this.state.sicCodes.filter((s, i) => i !== index)
+  onDelete(index) {
+    const sicCodesList = this.state.sicCodes.filter((s, i) => i !== index);
 
-        this.setState({
-            ...this.state,
-            sicCodes: sicCodesList,
-            count: this.state.count - 1
-        })
+    this.setState({
+      ...this.state,
+      sicCodes: sicCodesList,
+      count: this.state.count - 1,
+    });
 
-        this.forceUpdate()
-        this.props.onUpdate('sicCodes', sicCodesList)
-    }
+    this.forceUpdate();
+    this.props.onUpdate("sicCodes", sicCodesList);
+  }
 
-    onChange(index, key, value) {
-        this.state.sicCodes[index][key] = value
-        this.forceUpdate()
-        this.props.onUpdate('sicCodes', this.state.sicCodes)
-    }
+  onChange(index, key, value) {
+    this.state.sicCodes[index][key] = value;
+    this.forceUpdate();
+    this.props.onUpdate("sicCodes", this.state.sicCodes);
+  }
 
-    render() {
-        return (
-            <div>
-            <MultiOptionCard title="List your SIC Codes" onAdd={this.onAdd} min={this.state.min} max={this.state.max} count={this.state.count}>
-                {
-                    this.state.sicCodes.map((sicCode, index) => {
-                        return (
-                            <Card key={index}>
-                                <Form.Item
-                                    key={index}
-                                    label={`SIC Code ${index + 1}`}
-                                    prop={`sicCodes:${index}`}
-                                    rules={{
-                                        type: 'object',
-                                        fields: {
-                                            value: [{ required: true, message: 'SIC Code can not be empty', trigger: 'blur'} ,{ trigger: 'blur', validator: (rule, value, callback) => {
-                                            var sicPattern = /^\d{4}$/
-                                            if (!sicPattern.test(value)) {
-                                            callback(new Error('SIC# error: Invalid SIC number. Please Enter 4 digit SIC number'));
-                                            } else {
-                                            callback();
-                                            }}}]
-                                        }
-                                    }}
-                                >
-                                <SingleOptionCard onDelete={this.onDelete} index={index} disabledDelete={this.state.count <= this.state.min}>
-
-                                        <Input value={sicCode.value} onChange={(value) => this.onChange(index, 'value', value)}></Input>
-                                    </SingleOptionCard>
-                                </Form.Item>
-                            </Card>
-                        )
-                    })
-                }
-            </MultiOptionCard>
-            <div className="add-btn">
+  render() {
+    return (
+      <div>
+        <MultiOptionCard
+          title="List your SIC Codes"
+          onAdd={this.onAdd}
+          min={this.state.min}
+          max={this.state.max}
+          count={this.state.count}
+        >
+        <div className="form-link">
+            <a href="https://siccode.com/sic-code-lookup-directory" target="_blank">
+              SIC Codes
+            </a>
+          </div>
+          {this.state.sicCodes.map((sicCode, index) => {
+            return (
+              <Card key={index}>
+                <Form.Item
+                  key={index}
+                  label={`SIC Code ${index + 1}`}
+                  prop={`sicCodes:${index}`}
+                  rules={{
+                    type: "object",
+                    fields: {
+                      value: [
+                        {
+                          required: true,
+                          message: "SIC Code can not be empty",
+                          trigger: "blur",
+                        },
+                        {
+                          trigger: "blur",
+                          validator: (rule, value, callback) => {
+                            var sicPattern = /^\d{4}$/;
+                            if (!sicPattern.test(value)) {
+                              callback(
+                                new Error(
+                                  "SIC# error: Invalid SIC number. Please Enter 4 digit SIC number"
+                                )
+                              );
+                            } else {
+                              callback();
+                            }
+                          },
+                        },
+                      ],
+                    },
+                  }}
+                >
+                  <SingleOptionCard
+                    onDelete={this.onDelete}
+                    index={index}
+                    disabledDelete={this.state.count <= this.state.min}
+                  >
+                    <Input
+                      value={sicCode.value}
+                      onChange={(value) => this.onChange(index, "value", value)}
+                    ></Input>
+                  </SingleOptionCard>
+                </Form.Item>
+              </Card>
+            );
+          })}
+        </MultiOptionCard>
+        <div className="add-btn">
           <Button
             type="primary"
             icon="plus"
@@ -96,9 +125,8 @@ class SicFormItem extends Component {
           ></Button>
         </div>
       </div>
-        )
-    }
-
+    );
+  }
 }
 
-export default SicFormItem; 
+export default SicFormItem;
